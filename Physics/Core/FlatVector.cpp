e49749ba19d3a770cab2221587f3686d63004240
lvhung.dev <lvhung.dev@gmail.com>
2023-12-06 23:19:57 +0700

    Implement FlatVector

---
+++ Physics/Core/FlatVector.cpp
@@ -0,0 +1,113 @@
+#include <cmath>
+#include "FlatVector.h"
+
+FlatVector::FlatVector() = default;
+
+FlatVector::FlatVector(float x, float y)
+{
+	this->x = x;
+	this->y = y;
+}
+
+FlatVector FlatVector::operator+(const FlatVector& other) const
+{
+	return { x + other.x, y + other.y };
+}
+
+FlatVector FlatVector::operator+(const float other) const
+{
+	return { x + other, y + other };
+}
+
+FlatVector FlatVector::operator-(const FlatVector& other) const
+{
+	return { x - other.x, y - other.y };
+}
+
+FlatVector FlatVector::operator-(float other) const
+{
+	return { x - other, y - other };
+}
+
+FlatVector FlatVector::operator*(float other) const
+{
+	return { x * other, y * other };
+}
+
+FlatVector FlatVector::operator/(float other) const
+{
+	return { x / other, y / other };
+}
+
+void FlatVector::operator+=(const FlatVector& other)
+{
+	this->x += other.x;
+	this->y += other.y;
+}
+
+void FlatVector::operator+=(float other)
+{
+	this->x += other;
+	this->y += other;
+}
+
+void FlatVector::operator-=(const FlatVector& other)
+{
+	this->x -= other.x;
+	this->y -= other.y;
+}
+
+void FlatVector::operator-=(float other)
+{
+	this->x -= other;
+	this->y -= other;
+}
+
+void FlatVector::operator*=(float other)
+{
+	this->x *= other;
+	this->y *= other;
+}
+
+void FlatVector::operator/=(float other)
+{
+	if (other == 0) return;
+
+	this->x /= other;
+	this->y /= other;
+}
+
+float FlatVector::dotProduct(const FlatVector& other) const
+{
+	return this->x * other.x + this->y * other.y;
+}
+
+float FlatVector::getLength() const
+{
+	return sqrtf(this->x * this->x + this->y * this->y);
+}
+
+float FlatVector::getLengthSquared() const
+{
+	return this->x * this->x + this->y * this->y;
+}
+
+FlatVector FlatVector::getNormalized() const
+{
+	if (x == 0.0f && y == 0.0f) return { 0.0f, 0.0f };
+
+	return *this / getLength();
+}
+
+FlatVector FlatVector::getPerpendicular() const
+{
+	return FlatVector(-y, x);
+}
+
+FlatVector FlatVector::getRotated(float angle) const
+{
+	return FlatVector(
+		x * cosf(angle) - y * sinf(angle),
+		x * sinf(angle) + y * cosf(angle)
+	);
+}
---
+++ Physics/Core/FlatVector.h
@@ -0,0 +1,48 @@
+#pragma once
+
+class FlatVector
+{
+public:
+	FlatVector();
+
+	FlatVector(float x, float y);
+
+	float x = 0.0f;
+	float y = 0.0f;
+
+	FlatVector operator+(const FlatVector& other) const;
+
+	FlatVector operator+(float other) const;
+
+	FlatVector operator-(const FlatVector& other) const;
+
+	FlatVector operator-(float other) const;
+
+	FlatVector operator*(float other) const;
+
+	FlatVector operator/(float other) const;
+
+	void operator+=(const FlatVector& other);
+
+	void operator+=(float other);
+
+	void operator-=(const FlatVector& other);
+
+	void operator-=(float other);
+
+	void operator*=(float other);
+
+	void operator/=(float other);
+
+	float dotProduct(const FlatVector& other) const;
+
+	float getLength() const;
+
+	float getLengthSquared() const;
+
+	FlatVector getNormalized() const;
+
+	FlatVector getPerpendicular() const;
+
+	FlatVector getRotated(float angle) const;
+};
